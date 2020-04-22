b82ce8a49c2d26044686f2d2ed75833e1a21a27b
FairyEver <1711467488@qq.com>
2020-04-22 22:20:03 +0800

    feat: 修改演示页面

---
+++ src/views/class1/1/page.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    $route.name: {{ $route.name }}
+  </div>
+</template>
+
+<script>
+export default {
+  title: 'Class 1 - 1'
+}
+</script>
---
+++ src/views/class2/1/page.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    $route.name: {{ $route.name }}
+  </div>
+</template>
+
+<script>
+export default {
+  title: 'Class 2 - 1'
+}
+</script>
